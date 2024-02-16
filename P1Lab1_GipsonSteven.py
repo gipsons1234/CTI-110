ed99442201c7ed26b898048f2a0d1cf76ce7a66d
gipsons1234 <160270754+gipsons1234@users.noreply.github.com>
2024-02-16 13:15:53 -0500

    Add files via upload

---
+++ P1Lab1_GipsonSteven.py
@@ -0,0 +1,13 @@
+#Steven Gipson
+#2/16/24
+#P1 Lab 1
+#This assignment teaches students how to create a Python program
+
+# Prompt the user to enter their first name
+first_name = input("Enter your first name: ")
+
+# Create a welcome message using the entered name
+welcome_message = "Welcome, " + first_name + "!"
+
+# Output the welcome message
+print(welcome_message)
