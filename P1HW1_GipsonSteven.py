63d1072a9bf846772da02216db86dd4b9aaccf38
gipsons1234 <160270754+gipsons1234@users.noreply.github.com>
2024-02-24 12:19:12 -0500

    P1HW1
    This assignment This assignment demonstrates the ability to write code that collects information, processes collected information, and displays results.

---
+++ P1HW1_GipsonSteven.py
@@ -0,0 +1,24 @@
+
+# Steven Gipson
+# 2-24-2024
+# P1HW1
+# This assignment demonstrates the ability to write code that collects information, processes collected information, and displays results.
+
+
+
+
+
+# Prompt the user to enter the first integer
+num1 = int(input("Enter the integer: "))
+
+# Print the integer, its square, and its cube
+print(f"You entered{num1}")
+print(f"The square of the integer is {num1 ** 2}")
+print(f"The cube of the integer is {num1 ** 3}")
+
+# Prompt the user to enter the second integer
+num2 = int(input("Enter another integer: "))
+
+# Print the sum and product of the two integers
+print(f"{num1} + {num2} = {num1 + num2}")
+print(f"{num1} * {num2} = {num1 * num2}")
